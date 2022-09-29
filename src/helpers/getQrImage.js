071e20681e01ad155a3f74340abc0975c2c1eee3
Luis Leal <luisrrleal@gmail.com>
2022-09-28 22:20:00 -0500

    Tests in host

--- src/helpers/getQrImage.js
+++ src/helpers/getQrImage.js
@@ -1,4 +1,4 @@
 export const getQrImage = async (code) => {
-  const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${code}`;
+  const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${"http://192.168.1.69:5173/"+code}`;
   return apiUrl;
 };
